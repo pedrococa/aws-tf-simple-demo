36aafc4cb3c948ce7f1ec1e45754f3042a5882ce
pedrococa <pedro.coca@gmail.com>
2020-09-30 09:48:04 +0100

    EC2 instance added

--- main.tf
+++ main.tf
@@ -2,6 +2,32 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 resource "aws_vpc" "default" {
   cidr_block = var.vpc_cidr
 }
